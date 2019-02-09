2bd3672fe8fe2ff31c570885a0644041ebc911df
Arthur Derichard <arthur.derichard@etu.u-bordeaux.fr>
2019-02-09 21:42:42 +0100

    [feature](new)Implementation Router -> inoperative

--- src/main.js
+++ src/main.js
@@ -3,11 +3,40 @@ import Vuetify from 'vuetify' //vuetify pour rendre plus beau <3
 import 'vuetify/dist/vuetify.min.css'
 import app from './app.vue' //fichier app.vue local
 import MovieItemComponent from './components/movieitem.vue' //fichier movieitem
+import VueRouter from 'vue-router'
+
 Vue.component('movie-item', MovieItemComponent);
+
+Vue.use(VueRouter)
+
 Vue.use(Vuetify, {
   iconfont: 'mdi' // 'md' || 'mdi' || 'fa' || 'fa4'
 })
+
+//test route 
+const NotFound = { template: '<p>404 Page not found</p></script>' }
+const Home = { template: '<p>home page test</p>' }
+const About = { template: '<p>about page test</p>' }
+
+const routes = {
+  '/': Home,
+  '/about': About
+}
+
+
+
 new Vue({
   el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+
+  //test route
+  //render: h => h(this.ViewComponent)
   render: h => h(app)
 })
