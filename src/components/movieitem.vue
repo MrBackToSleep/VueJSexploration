e1665a7826ed7768cbec9e24b8e07c83e8751cfe
Arthur Derichard <arthur.derichard@etu.u-bordeaux.fr>
2019-02-09 18:00:49 +0100

    [feature](new) Delete + search movies

---
+++ src/components/movieitem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <!-- Contenu d'un item movie --> 
+        <p>{{movie.title}}</p>
+        <p>{{movie.year}}</p>
+        <p>{{movie.synopsys}}</p>
+        <button v-on:click='btnClicked'>X</button>
+        <button v-on:click='btnEdit'>Edit</button>
+    </div>
+</template>
+<script>
+export default {
+    props: ['movie'],
+    methods: {
+        btnClicked() {
+            this.$emit('btnclicked')
+        },
+        btnEdit() {
+            this.$emit('btnedit')
+        },
+    }
+    
+}
+</script>
